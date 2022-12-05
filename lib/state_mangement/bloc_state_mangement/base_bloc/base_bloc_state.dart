3a3a6d19fe6c648caff71a108f52febf32c86b7a
Majd <majed.dalloul79@gmail.com>
2022-12-05 03:27:49 +0300

    base bloc page implementation part 2

--- lib/state_mangement/bloc_state_mangement/base_bloc/base_bloc_state.dart
+++ lib/state_mangement/bloc_state_mangement/base_bloc/base_bloc_state.dart
@@ -5,6 +5,8 @@ class BaseBlocState with _$BaseBlocState {
   const factory BaseBlocState({
     required bool isLoading,
     required ContextCallback contextCallback,
+    BaseFailure? failure,
+    Object? unknownError,
   }) = _BaseBlocState;
 
   factory BaseBlocState.initial() => BaseBlocState(
