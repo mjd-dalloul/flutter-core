b04c70da9fc1fb9d89c9d3fe5e89c559bfd695b9
Majd <majed.dalloul79@gmail.com>
2022-12-05 03:27:57 +0300

    helper widgets for bloc

---
+++ lib/presentation/widgets/bloc_helper_widget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class BlocBuilderWidget<B extends StateStreamable<S>, S>
+    extends StatelessWidget {
+  const BlocBuilderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<B, S>(
+        buildWhen: buildWhen,
+        builder: (context, state) => buildWithState(context, state));
+  }
+
+  Widget buildWithState(BuildContext context, S state);
+
+  bool buildWhen(S p, S c) => true;
+}
+
+abstract class BlocProviderWidget<T extends StateStreamableSource<Object?>>
+    extends StatelessWidget {
+  const BlocProviderWidget({Key? key}) : super(key: key);
+
+  T get bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<T>(
+      create: (_) => bloc,
+      child: Builder(builder: (context2) {
+        return buildChild(context2);
+      }),
+    );
+  }
+
+  Widget buildChild(context);
+}
+
+abstract class BlocListenerWidget<B extends StateStreamable<S>, S>
+    extends StatelessWidget {
+  const BlocListenerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<B, S>(
+      listenWhen: listenWhen,
+      listener: listener,
+      child: buildChild(context),
+    );
+  }
+
+  void listener(context, state);
+
+  Widget buildChild(BuildContext context);
+
+  bool listenWhen(S p, S c) => true;
+}
+
+abstract class BlocConsumerWidget<B extends StateStreamable<S>, S>
+    extends StatelessWidget {
+  const BlocConsumerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<B, S>(
+      listenWhen: listenWhen,
+      listener: listener,
+      buildWhen: buildWhen,
+      builder: buildWithState,
+    );
+  }
+
+  void listener(context, state);
+
+  Widget buildChild(BuildContext context);
+
+  bool listenWhen(S p, S c) => true;
+
+  Widget buildWithState(BuildContext context, S state);
+
+  bool buildWhen(S p, S c) => true;
+}
