65ca67579d9ead331513f53c00b6b0678821a7cc
Majd <majed.dalloul79@gmail.com>
2022-12-29 01:33:20 +0200

    base repository bug fixing

--- lib/repository/base_repostiroy.dart
+++ lib/repository/base_repostiroy.dart
@@ -3,10 +3,10 @@ import 'dart:developer';
 import 'package:flutter_core/utils/data_model_wrapper.dart';
 import 'package:flutter_core/utils/failures/base_failure.dart';
 
-abstract class BaseRepository {
+class BaseRepository {
   Future<DataModelWrapper<T>> requestData<T>({
     required Future<DataModelWrapper<T>> Function() remoteCall,
-    Future<DataModelWrapper<int>> Function(T? data)? saveRemoteDataFunction,
+    Future<DataModelWrapper<int>> Function(T data)? saveRemoteDataFunction,
     bool forceUpdate = true,
     Future<DataModelWrapper<T>> Function()? localCall,
     void Function(int insertedRow)? onCacheSuccess,
@@ -36,7 +36,7 @@ abstract class BaseRepository {
 
   Future<DataModelWrapper<T>> _remoteObjectWrapper<T>({
     required Future<DataModelWrapper<T>> Function() remoteCall,
-    Future<DataModelWrapper<int>> Function(T? data)? saveRemoteDataFunction,
+    Future<DataModelWrapper<int>> Function(T data)? saveRemoteDataFunction,
     void Function(int insertedRow)? onCacheSuccess,
     void Function(BaseFailure failure)? onCacheFailure,
   }) async {
@@ -45,7 +45,7 @@ abstract class BaseRepository {
       if (res.isFailure) {
         log('<BaseRepository>: Could not save because network call did not go well');
       } else if (res.data != null) {
-        final saveResult = await saveRemoteDataFunction.call(res.data);
+        final saveResult = await saveRemoteDataFunction.call(res.data!);
         if (saveResult.isFailure) {
           log('<BaseRepository>: Could not save in database because ${saveResult.failure}');
           onCacheFailure?.call(saveResult.failure!);
