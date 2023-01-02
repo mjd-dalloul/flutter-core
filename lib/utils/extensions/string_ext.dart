d7de2a3fdc239925c9e606e56eee32cd2499e1ac
Majd <majed.dalloul79@gmail.com>
2023-01-02 14:07:31 +0300

    string extension

--- lib/utils/extensions/string_ext.dart
+++ lib/utils/extensions/string_ext.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' as intl;
 
 extension StringExt on String? {
   bool get isNullOrEmpty => this == null || this!.isEmpty;
@@ -23,7 +24,7 @@ extension StringExt on String? {
 
   Size textSize(
     TextStyle style,
-    TextDirection direction, [
+    TextDirection textDirection, [
     double textScaleFactor = 1,
   ]) =>
       (TextPainter(
@@ -32,7 +33,34 @@ extension StringExt on String? {
           style: style,
         ),
         textScaleFactor: textScaleFactor,
-        textDirection: direction,
+        textDirection: textDirection,
       )..layout())
           .size;
+
+  String getDayTh([String? locale]) =>
+      intl.DateFormat.d(locale).format(DateTime.parse(this!));
+
+  String getDayShortName([String? locale]) =>
+      intl.DateFormat.E(locale).format(DateTime.parse(this!));
+
+  String getDayFullName([String? locale]) =>
+      intl.DateFormat.EEEE(locale).format(DateTime.parse(this!));
+
+  String getMonthShortName([String? locale]) =>
+      intl.DateFormat.MMM(locale).format(DateTime.parse(this!));
+
+  String getMonthFullName([String? locale]) =>
+      intl.DateFormat.MMMM(locale).format(DateTime.parse(this!));
+
+  String getMonthTh([String? locale]) =>
+      intl.DateFormat.M(locale).format(DateTime.parse(this!));
+
+  String getMonthDayTh([String? locale]) =>
+      intl.DateFormat.Md(locale).format(DateTime.parse(this!));
+
+  String getDayNameWithDate([String? locale]) =>
+      intl.DateFormat.MEd(locale).format(DateTime.parse(this!));
+
+  String getDayNameMonthNameWithDate([String? locale]) =>
+      intl.DateFormat.MMMEd(locale).format(DateTime.parse(this!));
 }
