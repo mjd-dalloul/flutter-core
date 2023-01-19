91631f3868b04c43f892b3aa368202c16e13192e
Majd <majed.dalloul79@gmail.com>
2023-01-19 17:12:14 +0300

    bloc state mixins

---
+++ lib/presentation/mixins/bloc_state_mixin.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_core/state_mangement/bloc_state_management/base_bloc.dart';
+
+mixin BlocStateProviderMixin<S extends StatefulWidget, B extends BaseBloc>
+    on State<S> {
+  late B bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    bloc = context.read<B>();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    bloc = context.read<B>();
+  }
+}
